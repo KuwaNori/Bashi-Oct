d749c17f08178f67e5bdbf15ce398ac18073964b
KuwaNori <kuwanori4778@gmail.com>
2020-10-26 11:15:05 +0900

    システムの変更と画像の挿入

--- regist.php
+++ regist.php
@@ -7,21 +7,23 @@
   </head>
 <body>
 <?php
-if (isset($_POST['komanetid'])){
+if (isset($_POST['komanetid']) && isset($_POST['nickname'])){
   $komanetid=$_POST['komanetid'];
+  $nickname = $_POST['nickname'];
   $sql="select * from oct_users where student_num='". $komanetid . "';";
     $dbconn = pg_connect("host=localhost  dbname=ktsubasa283 user=ktsubasa283 password=FFNAN6bu") or die('Could not connect: ' . pg_last_error());
   $result = pg_query($sql) or die('Query failed: ' . pg_last_error());
   if(pg_num_rows($result)==0){
     $npw=substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'), 0, 8);
     $npwh=password_hash($npw, PASSWORD_BCRYPT);
-    $sql="insert into oct_users (student_num,password) values ('" . $komanetid . "','" . $npwh . "');";
+    $sql="insert into oct_users (student_num,password,nickname) values ('" . $komanetid . "','" . $npwh . "','".$nickname."');";
     pg_query($sql) or die('Query failed: ' . pg_last_error());
     echo '<p>ユーザ登録を完了しました</p>';
     $mailfr="ktsubasa283@gms.gdl.jp";
     $mailsb="ユーザ登録完了";
     $mailms="下記のとおりユーザ登録を完了しました。\n\n" .
       "   ID:" . $komanetid . "\n\n" .
+      "   Nickname:" . $nickname . "\n\n" .
       "   あなたのパスワードは、\n\n" .
       "   " . $npw . "\n\n" .
       "   です。このパスワードを使ってログインしてください。\n\n" .
